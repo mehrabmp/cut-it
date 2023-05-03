5738c2d209ec88bb7b2c7b3159c4a0b99476270f
Mehrab <mehrabmpdev@gmail.com>
2023-05-03 23:37:27 +0330

    chore: configure prettier and eslint (#5)
    * chore: configure prettier and eslint
    
    * fix: make eslint happy

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
+// export async function GET(request: Request) {
+//   return new Response('Hello, Next.js!')
+// }
+
+export {};

2f283e521b098b9730fee7d80442278cab327e22
Mehrab <mehrabmpdev@gmail.com>
2023-05-03 23:57:40 +0330

    chore: add env validation and example

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.mjs");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;

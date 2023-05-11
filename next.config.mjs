21b3808f93227c278e8bd039387be280675e9d42
Mehrab <mehrabmpdev@gmail.com>
2023-05-11 17:39:04 +0330

    chore(deps): stable app router finally (#15)
    * chore(deps): stable app router finally
    
    * chore(deps): fix drizzle-kit version

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@
 await import("./src/env.mjs");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

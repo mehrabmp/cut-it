02cf6af3c525008995fcb7f87aabdc624b40f444
Mehrab <mehrabmpdev@gmail.com>
2023-05-14 14:11:28 +0330

    ci: add env variables to main workflow (#16)
    * ci: add postgres service to main workflow
    
    * fix: add SKIP_ENV_VALIDATION=true to build step
    
    * fix: update next config
    
    * fix: update env variables in main workflow
    
    * fix: drop postgres service

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};

86c2aeef681bb487669f39e144e033981d7294c9
Mehrab <mehrabmpdev@gmail.com>
2023-05-16 20:21:52 +0330

    feat(ui): create homepage components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-empty-interface": ["off"],
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
-import { Button } from "@/components/ui/Button";
-import { db } from "@/server/db";
-import { links } from "@/server/db/schema";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default async function Home() {
-  const data = await db.select().from(links);
-
+export default function Home() {
   return (
-    <main className="">
-      <ul>
-        {data.map((link) => (
-          <Link key={link.id} href={link.url}>
-            {link.title}{" "}
-          </Link>
-        ))}
-      </ul>
-      <Button>Cut it</Button>
-    </main>
+    <div className="flex flex-col items-center justify-center gap-20 p-2">
+      <div className="mt-20 space-y-2 text-center">
+        <h1 className="text-4xl font-bold">Free URL Shortener</h1>
+        <h2 className="font-normal text-neutral-800">
+          Cut It is a free open source tool to shorten URLs and generate short
+          links
+        </h2>
+      </div>
+      <div className="flex w-80 items-center justify-center gap-2">
+        <div className="flex-1">
+          <Input placeholder="Enter the link here" />
+        </div>
+        <div className="">
+          <Button>Cut it</Button>
+        </div>
+      </div>
+    </div>
   );
 }

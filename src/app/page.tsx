2135ed5a59a4ee4c6caf7a19da6a77be185cdc68
Mehrab <mehrabmpdev@gmail.com>
2023-05-16 20:35:19 +0330

    fix: revert ui components path change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/button";
-import { Input } from "@/components/input";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (

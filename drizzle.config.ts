2e27f268aebcb37c3dd9a6a1b06f7628ef3b105b
Mehrab <mehrabmpdev@gmail.com>
2023-06-19 12:12:35 +0330

    chore(deps): update dependencies (#40)
    * chore(deps): update dependencies
    
    * fix: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "drizzle-kit";
+import { env } from "@/env.mjs";
 
 export default {
-  connectionString: process.env.POSTGRES_URL,
+  dbCredentials: {
+    connectionString: env.POSTGRES_URL,
+  },
   schema: "./src/server/db/schema.ts",
   out: "./drizzle",
 } satisfies Config;

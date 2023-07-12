0af13204ff1f184c2bcd73fd1ac0100f44cb8f1f
Mehrab <mehrabmpdev@gmail.com>
2023-07-12 20:35:55 +0330

    chore(deps): update dependencies (#59)
    * chore(deps): update dependencies
    
    * chore: update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
-
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-      rules: {
-        "@typescript-eslint/no-misused-promises": "off",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
   extends: [
-    "next/core-web-vitals",
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "next/core-web-vitals",
     "prettier",
   ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -35,9 +21,23 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-empty-interface": "off",
   },
+  overrides: [
+    {
+      extends: ["plugin:@typescript-eslint/recommended-type-checked"],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        "@typescript-eslint/no-misused-promises": "off",
+      },
+    },
+  ],
+  root: true,
 };
 
 module.exports = config;

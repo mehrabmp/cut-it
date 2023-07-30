b6640a50b9a894d99d78443d776c5bc8b1ae0ce7
Mehrab <mehrabmpdev@gmail.com>
2023-07-30 18:07:29 +0330

    chore: update eslint rules (#73)
    * chore: update eslint rules
    
    * chore: update rules
    
    * chore: more eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,15 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "next/core-web-vitals",
-    "prettier",
-  ],
-  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
-    tsconfigRootDir: __dirname,
   },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -21,23 +18,11 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-empty-interface": "off",
   },
-  overrides: [
-    {
-      extends: ["plugin:@typescript-eslint/recommended-type-checked"],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
-      },
-      rules: {
-        "@typescript-eslint/no-misused-promises": "off",
-      },
-    },
-  ],
-  root: true,
 };
 
 module.exports = config;

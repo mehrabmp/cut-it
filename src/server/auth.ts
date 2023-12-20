5686bee6520e49a6642cf8b44a063075d6f45923
MehrabMP <mehrabmpdev@gmail.com>
2023-12-20 14:06:45 +0330

    refactor: update next-auth module and route

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,12 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { env } from "~/env";
+import { db } from "~/server/db";
 import {
   getServerSession,
-  type NextAuthOptions,
   type DefaultSession,
+  type NextAuthOptions,
 } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { env } from "@/env.mjs";
-import { db } from "./db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -16,11 +16,11 @@ import { db } from "./db";
  */
 declare module "next-auth" {
   interface Session extends DefaultSession {
-    user: DefaultSession["user"] & {
+    user: {
       id: string;
       // ...other properties
       // role: UserRole;
-    };
+    } & DefaultSession["user"];
   }
 
   // interface User {
@@ -67,6 +67,4 @@ export const authOptions: NextAuthOptions = {
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => {
-  return getServerSession(authOptions);
-};
+export const getServerAuthSession = () => getServerSession(authOptions);

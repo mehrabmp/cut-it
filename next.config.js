9333709b5aab14b24f250bdb4cada1c58afbb7ca
MehrabMP <mehrabmpdev@gmail.com>
2023-12-20 12:29:57 +0330

    chore: update next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
+
+export default config;

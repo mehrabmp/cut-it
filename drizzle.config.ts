29cc784790aabd8707d453f4d004c80c0a3cc0de
MehrabMP <mehrabmpdev@gmail.com>
2023-12-20 13:15:06 +0330

    refactor: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,12 @@
 import type { Config } from "drizzle-kit";
-import dotenv from "dotenv";
-dotenv.config();
+import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  out: "./drizzle",
   driver: "turso",
   dbCredentials: {
-    url: process.env.TURSO_URL ?? "",
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: env.TURSO_URL ?? "",
+    authToken: env.TURSO_AUTH_TOKEN,
     // url: "file:./local.db",
   },
   verbose: true,
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,1 +1,12 @@
-export { db } from "./db";
+import { createClient } from "@libsql/client/web";
+import { env } from "~/env";
+import { drizzle } from "drizzle-orm/libsql";
+
+import * as schema from "./schema";
+
+const client = createClient({
+  url: env.TURSO_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
+export const db = drizzle(client, { schema, logger: true });

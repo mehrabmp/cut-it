de86b1edec7bcbda8293cd08ecce243c188e3da4
MehrabMP <mehrabmpdev@gmail.com>
2023-12-22 18:51:00 +0330

    feat: add CopyToClipboard component

---
+++ src/components/links/copy-to-clipboard.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { toast } from "sonner";
+
+import { Icons, iconVariants } from "~/components/ui/icon";
+
+type CopyToClipboardProps = {
+  textToCopy: string;
+};
+
+export const CopyToClipboard = ({ textToCopy }: CopyToClipboardProps) => {
+  const handleOnCopy = async () => {
+    await navigator.clipboard.writeText(textToCopy);
+    toast("Copied to clipboard");
+  };
+
+  return (
+    <div className="cursor-pointer transition-colors hover:text-foreground">
+      <Icons.Copy
+        className={iconVariants({ size: "sm" })}
+        onClick={handleOnCopy}
+      />
+    </div>
+  );
+};
--- src/components/links/link-card.tsx
+++ src/components/links/link-card.tsx
@@ -1,12 +1,10 @@
-"use client";
-
 import Link from "next/link";
-// import type { Link } from "~/server/db/schema";
-import { toast } from "sonner";
 
 import { getBaseUrl } from "~/lib/utils";
 import { Card, CardContent } from "~/components/ui/card";
 import { Icons, iconVariants } from "~/components/ui/icon";
+
+import { CopyToClipboard } from "./copy-to-clipboard";
 
 export const LinkCard = ({ slug, url, viewCount }: Link) => {
   const decodedURL = decodeURIComponent(url);
@@ -14,13 +12,6 @@ export const LinkCard = ({ slug, url, viewCount }: Link) => {
   const numberFormatter = new Intl.NumberFormat("en-US", {
     notation: "compact",
   });
-
-  const handleOnCopyLink = async () => {
-    await navigator.clipboard.writeText(shortenedURL);
-    toast("Copied to clipboard");
-  };
-
-  const iconProps = iconVariants({ size: "sm" });
 
   return (
     <Card className="relative font-mono hover:bg-neutral-900 transition-colors">
@@ -35,11 +26,9 @@ export const LinkCard = ({ slug, url, viewCount }: Link) => {
             {shortenedURL.split("://")[1]}
           </Link>
           <div className="flex items-center gap-2 text-neutral-500">
-            <div className="cursor-pointer transition-colors hover:text-foreground">
-              <Icons.Copy className={iconProps} onClick={handleOnCopyLink} />
-            </div>
+            <CopyToClipboard textToCopy={shortenedURL} />
             <div className="flex cursor-pointer items-center gap-1 transition-colors hover:text-foreground">
-              <Icons.Eye className={iconProps} />
+              <Icons.Eye className={iconVariants({ size: "sm" })} />
               <span className="text-xs">
                 {numberFormatter.format(viewCount)}
               </span>

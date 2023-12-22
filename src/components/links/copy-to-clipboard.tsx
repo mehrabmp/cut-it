f7595a09006b74af9a1f41d78bc2e8793e29c701
MehrabMP <mehrabmpdev@gmail.com>
2023-12-22 20:05:42 +0330

    feat: add Tooltip component to UI

--- src/components/links/copy-to-clipboard.tsx
+++ src/components/links/copy-to-clipboard.tsx
@@ -3,6 +3,11 @@
 import { toast } from "sonner";
 
 import { Icons, iconVariants } from "~/components/ui/icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
 
 type CopyToClipboardProps = {
   textToCopy: string;
@@ -15,11 +20,18 @@ export const CopyToClipboard = ({ textToCopy }: CopyToClipboardProps) => {
   };
 
   return (
-    <div className="cursor-pointer transition-colors hover:text-foreground">
-      <Icons.Copy
-        className={iconVariants({ size: "sm" })}
-        onClick={handleOnCopy}
-      />
-    </div>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className="cursor-pointer transition-colors hover:text-foreground">
+          <Icons.Copy
+            className={iconVariants({ size: "sm" })}
+            onClick={handleOnCopy}
+          />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p className="font-sans">Copy link to clipboard</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,13 +2,15 @@
 
 import { SessionProvider } from "next-auth/react";
 
+import { TooltipProvider } from "~/components/ui/tooltip";
+
 import { ThemeProvider } from "./theme-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
       </ThemeProvider>
     </SessionProvider>
   );

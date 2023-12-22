f0c078e8ff6621f01e15ff7e7d9b2b9a58ae052b
MehrabMP <mehrabmpdev@gmail.com>
2023-12-22 19:34:00 +0330

    refactor: update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 
+import { Heading } from "~/components/ui/heading";
 import { Loader } from "~/components/ui/loader";
 import { LinkList } from "~/components/link-list";
 import { PublicLinkForm } from "~/components/public-link-form";
@@ -8,17 +9,18 @@ import { createShortLink } from "./actions";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-2">
-      <div className="my-20 space-y-2 text-center">
-        <h1 className="text-4xl font-bold">Free URL Shortener</h1>
-        <h2 className="font-normal text-neutral-400">
-          Cut It is a free open source tool to shorten URLs and generate short
-          links
-        </h2>
+    <div className="flex flex-col items-center p-4">
+      <div className="my-10 space-y-2 text-center">
+        <Heading variant="h1" isFirstBlock>
+          Free URL Shortener
+        </Heading>
+        <Heading variant="h2" className="text-muted-foreground">
+          Cut It is a free open source tool to generate short links
+        </Heading>
       </div>
-      <div className="flex w-full max-w-sm flex-col items-center space-y-6">
+      <div className="flex items-center w-full max-w-md flex-col gap-4">
         <PublicLinkForm createShortLink={createShortLink} />
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={<Loader className="my-20" />}>
           <LinkList />
         </Suspense>
       </div>

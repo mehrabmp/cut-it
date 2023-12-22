ad5c1eb45a3c265af19fd254f14e2031f581f912
MehrabMP <mehrabmpdev@gmail.com>
2023-12-22 18:29:54 +0330

    refactor: update LinkCard component

--- src/components/link-card.tsx
+++ src/components/link-card.tsx
@@ -1,52 +1,63 @@
 "use client";
 
-import { CopyIcon, DotsVerticalIcon, EyeOpenIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 // import type { Link } from "~/server/db/schema";
 import { toast } from "sonner";
 
+import { getBaseUrl } from "~/lib/utils";
 import { Card, CardContent } from "~/components/ui/card";
+import { Icons, iconVariants } from "~/components/ui/icon";
 
 export const LinkCard = ({ slug, url, viewCount }: Link) => {
   const decodedURL = decodeURIComponent(url);
+  const shortenedURL = `${getBaseUrl()}/${slug}`;
+  const numberFormatter = new Intl.NumberFormat("en-US", {
+    notation: "compact",
+  });
 
-  const shortLink = `${
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/"
-      : "https://cutit.vercel.app/"
-  }${slug}`;
-
-  const handleOnCopy = async () => {
-    await navigator.clipboard.writeText(shortLink);
+  const handleOnCopyLink = async () => {
+    await navigator.clipboard.writeText(shortenedURL);
     toast("Copied to clipboard");
   };
 
+  const iconProps = iconVariants({ size: "sm" });
+
   return (
-    <Card className="relative w-full">
-      <CardContent className="space-y-1 p-3">
-        <div className="flex items-center space-x-2">
-          <h3 className="text-sm">
-            <a href={shortLink} target="_blank" rel="noopener noreferrer">
-              {shortLink.split("://")[1]}
-            </a>
-          </h3>
-          <div className="flex items-center space-x-2.5 text-neutral-500">
-            <CopyIcon
-              className="cursor-pointer transition-colors hover:text-foreground"
-              onClick={handleOnCopy}
-            />
-            <div className="flex cursor-pointer items-center space-x-1 transition-colors hover:text-foreground">
-              <EyeOpenIcon />
-              <span className="text-xs">{viewCount}</span>
+    <Card className="relative font-mono hover:bg-neutral-900 transition-colors">
+      <CardContent className="flex flex-col gap-1 p-3">
+        <div className="flex items-center gap-2">
+          <Link
+            href={shortenedURL}
+            className="font-medium"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {shortenedURL.split("://")[1]}
+          </Link>
+          <div className="flex items-center gap-2 text-neutral-500">
+            <div className="cursor-pointer transition-colors hover:text-foreground">
+              <Icons.Copy className={iconProps} onClick={handleOnCopyLink} />
+            </div>
+            <div className="flex cursor-pointer items-center gap-1 transition-colors hover:text-foreground">
+              <Icons.Eye className={iconProps} />
+              <span className="text-xs">
+                {numberFormatter.format(viewCount)}
+              </span>
             </div>
           </div>
         </div>
-        <h4 className="truncate pr-4 text-xs text-neutral-500">
-          <a href={decodedURL} target="_blank" rel="noopener noreferrer">
+        <div className="truncate pr-4 text-xs text-neutral-500">
+          <Link href={decodedURL} target="_blank" rel="noopener noreferrer">
             {decodedURL}
-          </a>
-        </h4>
+          </Link>
+        </div>
       </CardContent>
-      <DotsVerticalIcon className="absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-neutral-500 transition-colors hover:text-foreground" />
+      <Icons.MoreVertical
+        className={iconVariants({
+          className:
+            "absolute right-2 top-[50%] translate-y-[-50%] cursor-pointer text-neutral-500 transition-colors hover:text-foreground",
+        })}
+      />
     </Card>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function getBaseUrl() {
+  if (typeof window !== "undefined") return "";
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export const nanoid = customAlphabet(
   "1234567890abcdefabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
   6,

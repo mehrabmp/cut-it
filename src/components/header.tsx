106f6a59363689dde8ae997046e9f7ae9f639942
MehrabMP <mehrabmpdev@gmail.com>
2023-12-22 19:28:24 +0330

    feat: add GitHub link to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 import { Paytone_One } from "next/font/google";
+import Link from "next/link";
 
 import { Button } from "~/components/ui/button";
+import { Icons, iconVariants } from "~/components/ui/icons";
 
 import { ThemeToggle } from "./theme-toggle";
 
@@ -11,6 +13,17 @@ export const Header = () => {
     <header className="mx-auto flex w-full max-w-5xl items-center justify-between p-4">
       <h1 className={`${paytoneOne.className} text-3xl`}>Cut it</h1>
       <div className="flex items-center gap-2">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-muted-foreground transition-colors hover:text-foreground"
+          asChild
+        >
+          <Link href="https://github.com/mehrabmp/cut-it" target="_blank">
+            <Icons.github className={iconVariants({ size: "lg" })} />
+            <span className="sr-only">github repository</span>
+          </Link>
+        </Button>
         <ThemeToggle />
         <Button>Sign in</Button>
       </div>

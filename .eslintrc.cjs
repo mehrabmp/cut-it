bb67b68421c7b4f2ecf4b075c2c164a0e10c2617
MehrabMP <mehrabmpdev@gmail.com>
2023-12-24 14:21:47 +0330

    chore: add drizzle plugin to ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "drizzle"],
   extends: [
     "plugin:@next/next/recommended",
+    "plugin:drizzle/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],

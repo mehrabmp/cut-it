eb0054ca88d46e10671ec8a5555c9b5db043ceb2
MehrabMP <mehrabmpdev@gmail.com>
2023-12-28 00:00:24 +0330

    refactor: fetch short links with link-id

--- src/components/links/link-list.tsx
+++ src/components/links/link-list.tsx
@@ -1,16 +1,21 @@
-import { headers } from "next/headers";
-import { db } from "~/server/db";
+import { cookies } from "next/headers";
+import { getShortLinksByLinkId } from "~/server/api/link";
 
 import { LinkCard } from "./link-card";
 
 export const LinkList = async () => {
-  headers();
+  const cookieStore = cookies();
+  const linkId = cookieStore.get("link-id");
 
-  const links = await db.query.links.findMany({});
+  if (!linkId) {
+    return null;
+  }
+
+  const shortLinks = await getShortLinksByLinkId(linkId.value);
 
   return (
     <div className="flex w-full flex-col gap-2">
-      {links.map((link) => (
+      {shortLinks.map((link) => (
         <LinkCard key={link.id} {...link} />
       ))}
     </div>

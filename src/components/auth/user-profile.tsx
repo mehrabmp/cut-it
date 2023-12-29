8ca8a2959245f4a267ce32f2ae9e8ac8839402ec
MehrabMP <mehrabmpdev@gmail.com>
2023-12-29 12:30:56 +0330

    fix: resolve bug in user profile component

--- src/components/auth/user-profile.tsx
+++ src/components/auth/user-profile.tsx
@@ -1,5 +1,4 @@
 import { getServerAuthSession } from "~/server/auth";
-import { type User } from "next-auth";
 
 import { Button } from "~/components/ui/button";
 
@@ -8,13 +7,14 @@ import { UserProfileDropdown } from "./user-profile-dropdown";
 
 export const UserProfile = async () => {
   const session = await getServerAuthSession();
-  const user = session?.user;
 
-  if (!user) {
-    <SigninDialog>
-      <Button size="sm">Sign In</Button>
-    </SigninDialog>;
+  if (!session) {
+    return (
+      <SigninDialog>
+        <Button size="sm">Sign In</Button>
+      </SigninDialog>
+    );
   }
 
-  return <UserProfileDropdown user={user as User} />;
+  return <UserProfileDropdown user={session.user} />;
 };

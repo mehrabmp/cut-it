35820fdf12241bb165bad76a8b5d643bb73394eb
MehrabMP <mehrabmpdev@gmail.com>
2023-12-31 21:08:08 +0330

    feat: add authAction middleware to handle server authentication

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,3 +1,16 @@
+import { getServerAuthSession } from "~/server/auth";
 import { createSafeActionClient } from "next-safe-action";
 
 export const action = createSafeActionClient();
+
+export const authAction = createSafeActionClient({
+  async middleware() {
+    const session = await getServerAuthSession();
+
+    if (!session) {
+      throw new Error("Session not found!");
+    }
+
+    return { user: session.user };
+  },
+});

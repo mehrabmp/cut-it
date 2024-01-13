0fe0d5485f4a130d04fc9583e01bcb7c68b5dd3a
MehrabMP <mehrabmpdev@gmail.com>
2024-01-14 00:13:04 +0330

    refactor: update custom link dialog component

--- src/components/links/custom-link-dialog.tsx
+++ src/components/links/custom-link-dialog.tsx
@@ -3,24 +3,17 @@
 import React, { useState } from "react";
 import { type ShortLink } from "~/server/db/schema";
 
-import { useMediaQuery } from "~/hooks/use-media-query";
 import { Button } from "~/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "~/components/ui/drawer";
+  ResponsiveDialog,
+  ResponsiveDialogBody,
+  ResponsiveDialogClose,
+  ResponsiveDialogContent,
+  ResponsiveDialogFooter,
+  ResponsiveDialogHeader,
+  ResponsiveDialogTitle,
+  ResponsiveDialogTrigger,
+} from "~/components/ui/responsive-dialog";
 import { CustomLinkButton } from "~/components/links/custom-link-button";
 import { CustomLinkForm } from "~/components/links/custom-link-form";
 
@@ -53,7 +46,6 @@ export const CustomLinkDialog = ({
 }: CustomLinkDialogProps) => {
   const [isOpen, setIsOpen] = useState(open);
   const isControlled = onOpenChange !== undefined;
-  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   const handleOpenChange = (isOpen: boolean) => {
     if (!isControlled) {
@@ -64,58 +56,32 @@ export const CustomLinkDialog = ({
 
   const openState = isControlled ? open : isOpen;
 
-  if (isDesktop) {
-    return (
-      <Dialog open={openState} onOpenChange={handleOpenChange}>
-        {!isEditing && (
-          <DialogTrigger asChild>
-            <CustomLinkButton />
-          </DialogTrigger>
-        )}
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>
-              {isEditing ? "Edit link" : "Create a new link"}
-            </DialogTitle>
-          </DialogHeader>
-          <div className="pt-4">
-            <CustomLinkForm
-              onSetIsDialogOpen={handleOpenChange}
-              defaultValues={defaultValues}
-              isEditing={isEditing}
-            />
-          </div>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
   return (
-    <Drawer open={openState} onOpenChange={handleOpenChange}>
+    <ResponsiveDialog open={openState} onOpenChange={handleOpenChange}>
       {!isEditing && (
-        <DrawerTrigger asChild>
+        <ResponsiveDialogTrigger asChild>
           <CustomLinkButton />
-        </DrawerTrigger>
+        </ResponsiveDialogTrigger>
       )}
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>
+      <ResponsiveDialogContent>
+        <ResponsiveDialogHeader>
+          <ResponsiveDialogTitle>
             {isEditing ? "Edit link" : "Create a new link"}
-          </DrawerTitle>
-        </DrawerHeader>
-        <div className="px-4 pt-4">
+          </ResponsiveDialogTitle>
+        </ResponsiveDialogHeader>
+        <ResponsiveDialogBody>
           <CustomLinkForm
             onSetIsDialogOpen={handleOpenChange}
             defaultValues={defaultValues}
             isEditing={isEditing}
           />
-        </div>
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
+        </ResponsiveDialogBody>
+        <ResponsiveDialogFooter>
+          <ResponsiveDialogClose asChild>
             <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          </ResponsiveDialogClose>
+        </ResponsiveDialogFooter>
+      </ResponsiveDialogContent>
+    </ResponsiveDialog>
   );
 };

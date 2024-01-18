a2d2e90f8037c30fe7d6d43a8c1ec6357ca4497b
MehrabMP <mehrabmpdev@gmail.com>
2024-01-18 23:28:23 +0330

    refactor: add DeleteLinkDialog component

---
+++ src/components/links/delete-link-dialog.tsx
@@ -0,0 +1,64 @@
+import { deleteShortLink } from "~/server/actions/link";
+import { useAction } from "next-safe-action/hooks";
+import { toast } from "sonner";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~/components/ui/alert-dialog";
+import { Button } from "~/components/ui/button";
+
+type DeleteLinkDialogProps = {
+  slug: string;
+  isOpen?: boolean;
+  onOpenChange?: (isOpen: boolean) => void;
+};
+
+export const DeleteLinkDialog = ({
+  slug,
+  isOpen,
+  onOpenChange,
+}: DeleteLinkDialogProps) => {
+  const { execute: deleteLink, status: deleteLinkStatus } = useAction(
+    deleteShortLink,
+    {
+      onSuccess() {
+        toast.info("Link deleted successfully");
+        onOpenChange?.(false);
+      },
+      onError(error) {
+        toast.error(error.serverError);
+      },
+    },
+  );
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={onOpenChange}>
+      <AlertDialogContent className="max-w-[22rem] sm:max-w-lg">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the link.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <Button
+            variant="destructive"
+            onClick={() => deleteLink({ slug })}
+            isLoading={deleteLinkStatus === "executing"}
+          >
+            {deleteLinkStatus === "executing"
+              ? "Deleting link..."
+              : "Delete link"}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- src/components/links/link-options-dropdown.tsx
+++ src/components/links/link-options-dropdown.tsx
@@ -1,23 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { deleteShortLink } from "~/server/actions/link";
 import { type ShortLink } from "~/server/db/schema";
 import { type Session } from "next-auth";
-import { useAction } from "next-safe-action/hooks";
-import { toast } from "sonner";
 
 import { cn } from "~/lib/utils";
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "~/components/ui/alert-dialog";
-import { Button } from "~/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -25,9 +12,9 @@ import {
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 import { Icons, iconVariants } from "~/components/ui/icons";
-
-import { CustomLinkDialog } from "./custom-link-dialog";
-import { LinkQRCodeDialog } from "./link-qrcode-dialog";
+import { CustomLinkDialog } from "~/components/links/custom-link-dialog";
+import { DeleteLinkDialog } from "~/components/links/delete-link-dialog";
+import { LinkQRCodeDialog } from "~/components/links/link-qrcode-dialog";
 
 const LinkOptionsDropdown = React.forwardRef<
   React.ElementRef<typeof DropdownMenuTrigger>,
@@ -42,19 +29,6 @@ const LinkOptionsDropdown = React.forwardRef<
     React.useState(false);
 
   const decodedURL = decodeURIComponent(link.url);
-
-  const { execute: deleteLink, status: deleteLinkStatus } = useAction(
-    deleteShortLink,
-    {
-      onSuccess() {
-        toast.info("Link deleted successfully");
-        setIsDeleteDialogOpen(false);
-      },
-      onError(error) {
-        toast.error(error.serverError);
-      },
-    },
-  );
 
   return (
     <>
@@ -100,32 +74,11 @@ const LinkOptionsDropdown = React.forwardRef<
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-      <AlertDialog
-        open={isDeleteDialogOpen}
+      <DeleteLinkDialog
+        isOpen={isDeleteDialogOpen}
         onOpenChange={setIsDeleteDialogOpen}
-      >
-        <AlertDialogContent className="max-w-[22rem] sm:max-w-lg">
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete the
-              link.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <Button
-              variant="destructive"
-              onClick={() => deleteLink({ slug: link.slug })}
-              isLoading={deleteLinkStatus === "executing"}
-            >
-              {deleteLinkStatus === "executing"
-                ? "Deleting link..."
-                : "Delete link"}
-            </Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+        slug={link.slug}
+      />
       <CustomLinkDialog
         open={isEditLinkDialogOpen}
         onOpenChange={setIsEditLinkDialogOpen}
